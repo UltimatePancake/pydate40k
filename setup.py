b3945f05a317ab3d49e734b516c947e6271b12c9
UltimatePancake <pier.gaetani@gmail.com>
2019-12-11 00:01:50 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(name='pydate40k',
       version='0.1',
       url='https://github.com/UltimatePancake/pydate40k',
@@ -8,7 +11,7 @@ setup(name='pydate40k',
       author_email='pier.gaetani@gmail.com',
       description='Return a date in the format of the Imperial Calendar',
       packages=find_packages(exclude=['tests']),
-      long_description=open('README.md', 'r').read(),
+      long_description=long_description,
       long_description_content_type='text/markdown',
       python_requires='>=3.7',
       classifiers=[
